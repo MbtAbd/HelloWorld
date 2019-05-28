587552aac30d69cf51f0b589a3cf1d161655e2c8
AbderrahmenM <abderrahmenmrabet1@gmail.com>
2019-05-28 12:19:59 +0200

    Import HW.py

---
+++ HW/HelloWorld.py
@@ -0,0 +1,18 @@
+"""
+HelloWorld.py
+====================================
+Document créé à 12h20.
+"""
+
+def confessions(your_name):
+    """
+    Return the most important secret of a person.
+    
+    Parameters
+    ----------
+    your_name
+        A string indicating the name of the person.
+    """
+    print("The wise {} loves Python.".format(your_name))            
+
+confessions("Ab")
